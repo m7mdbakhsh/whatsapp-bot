312b7aba304160625d9088234fb670a329cc0ef0
m7mdbakhsh <maabakhsh@kau.edu.sa>
2025-07-21 14:50:59 +0300

    Update app.py
    ok

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import os
 
 app = Flask(__name__)
 
@@ -39,4 +40,5 @@ def whatsapp_reply():
     return str(response)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
